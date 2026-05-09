31a8194449221f4f733a0d38348489e49e3ea111
ols20034 <ols20034@byui.edu>
2026-05-08 20:29:02 -0600

    W03 responsive design

---
+++ ponder/responsive.js
@@ -0,0 +1,10 @@
+const btn = document.querySelector('.menu-btn');
+const menu= document.querySelector('nav');
+
+
+btn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    menu.classList.toggle('hide');
+    btn.classList.toggle('change');
+}
